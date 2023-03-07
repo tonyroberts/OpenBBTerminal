76aa1899184543fe59e9bb62f19af46f639cd522
Diogo Sousa <montezdesousa@gmail.com>
2023-03-07 16:19:51 +0000

    automate menu gen

--- openbb_terminal/featflags_controller.py
+++ openbb_terminal/featflags_controller.py
@@ -45,33 +45,15 @@ class FeatureFlagsController(BaseController):
         mt = MenuText("featflags/")
         mt.add_info("_info_")
         mt.add_raw("\n")
-        # mt.add_setting("retryload", current_user.preferences.RETRY_WITH_LOAD)
-        # mt.add_setting("tab", current_user.preferences.USE_TABULATE_DF)
-        # mt.add_setting("cls", current_user.preferences.USE_CLEAR_AFTER_CMD)
-        # mt.add_setting("color", current_user.preferences.USE_COLOR)
-        # mt.add_setting("promptkit", current_user.preferences.USE_PROMPT_TOOLKIT)
-        # mt.add_setting("thoughts", current_user.preferences.ENABLE_THOUGHTS_DAY)
-        # mt.add_setting("reporthtml", current_user.preferences.OPEN_REPORT_AS_HTML)
-        # mt.add_setting("exithelp", current_user.preferences.ENABLE_EXIT_AUTO_HELP)
-        # mt.add_setting("rcontext", current_user.preferences.REMEMBER_CONTEXTS)
-        # mt.add_setting("rich", current_user.preferences.ENABLE_RICH)
-        # mt.add_setting("richpanel", current_user.preferences.ENABLE_RICH_PANEL)
-        # mt.add_setting("ion", current_user.preferences.USE_ION)
-        # mt.add_setting("watermark", current_user.preferences.USE_WATERMARK)
-        # mt.add_setting("cmdloc", current_user.preferences.USE_CMD_LOCATION_FIGURE)
-        # mt.add_setting("tbhint", current_user.preferences.TOOLBAR_HINT)
-        # mt.add_setting("overwrite", current_user.preferences.FILE_OVERWRITE)
 
         # automatically add the settings  by looping over the CHOICES_COMMANDS and the preferences model
-        for choice in self.CHOICES_COMMANDS:
-            attribute = getattr(current_user.preferences, choice)
-            print(attribute)
-            try:
-                help = attribute.metadata.get("help")
-            except Exception as e:
-                print(e)
-            # access the attribute field
-            mt.add_raw(f"{choice}: {attribute} {help}\n")
+        for (
+            pref_name,
+            pref_field,
+        ) in current_user.preferences.__dataclass_fields__.items():
+            help = pref_field.metadata.get("help")
+            pref_value = getattr(current_user.preferences, pref_name)
+            mt.add_raw(f"{pref_name}: {pref_value} {help}\n")
 
         console.print(text=mt.menu_text, menu="Feature Flags")
 
@@ -79,121 +61,4 @@ class FeatureFlagsController(BaseController):
         """Process overwrite command"""
         set_preference(
             "OPENBB_FILE_OVERWITE", not get_current_user().preferences.FILE_OVERWRITE
-        )
-
-    def call_retryload(self, _):
-        """Process retryload command"""
-        set_preference(
-            "OPENBB_RETRY_WITH_LOAD", not get_current_user().preferences.RETRY_WITH_LOAD
-        )
-
-    @log_start_end(log=logger)
-    def call_tab(self, _):
-        """Process tab command"""
-        set_preference(
-            "OPENBB_USE_TABULATE_DF", not get_current_user().preferences.USE_TABULATE_DF
-        )
-
-    @log_start_end(log=logger)
-    def call_cls(self, _):
-        """Process cls command"""
-        set_preference(
-            "OPENBB_USE_CLEAR_AFTER_CMD",
-            not get_current_user().preferences.USE_CLEAR_AFTER_CMD,
-        )
-
-    @log_start_end(log=logger)
-    def call_color(self, _):
-        """Process color command"""
-        set_preference("OPENBB_USE_COLOR", not get_current_user().preferences.USE_COLOR)
-
-    @log_start_end(log=logger)
-    def call_promptkit(self, _):
-        """Process promptkit command"""
-        set_preference(
-            "OPENBB_USE_PROMPT_TOOLKIT",
-            not get_current_user().preferences.USE_PROMPT_TOOLKIT,
-        )
-
-    @log_start_end(log=logger)
-    def call_thoughts(self, _):
-        """Process thoughts command"""
-        set_preference(
-            "OPENBB_ENABLE_THOUGHTS_DAY",
-            not get_current_user().preferences.ENABLE_THOUGHTS_DAY,
-        )
-
-    @log_start_end(log=logger)
-    def call_reporthtml(self, _):
-        """Process reporthtml command"""
-        set_preference(
-            "OPENBB_OPEN_REPORT_AS_HTML",
-            not get_current_user().preferences.OPEN_REPORT_AS_HTML,
-        )
-
-    @log_start_end(log=logger)
-    def call_exithelp(self, _):
-        """Process exithelp command"""
-        set_preference(
-            "OPENBB_ENABLE_EXIT_AUTO_HELP",
-            not get_current_user().preferences.ENABLE_EXIT_AUTO_HELP,
-        )
-
-    @log_start_end(log=logger)
-    def call_rcontext(self, _):
-        """Process rcontext command"""
-        set_preference(
-            "OPENBB_REMEMBER_CONTEXTS",
-            not get_current_user().preferences.REMEMBER_CONTEXTS,
-        )
-
-    @log_start_end(log=logger)
-    def call_dt(self, _):
-        """Process dt command"""
-        set_preference(
-            "OPENBB_USE_DATETIME", not get_current_user().preferences.USE_DATETIME
-        )
-
-    @log_start_end(log=logger)
-    def call_rich(self, _):
-        """Process rich command"""
-        set_preference(
-            "OPENBB_ENABLE_RICH", not get_current_user().preferences.ENABLE_RICH
-        )
-
-    @log_start_end(log=logger)
-    def call_richpanel(self, _):
-        """Process richpanel command"""
-        set_preference(
-            "OPENBB_ENABLE_RICH_PANEL",
-            not get_current_user().preferences.ENABLE_RICH_PANEL,
-        )
-
-    @log_start_end(log=logger)
-    def call_ion(self, _):
-        """Process ion command"""
-        set_preference("OPENBB_USE_ION", not get_current_user().preferences.USE_ION)
-
-    @log_start_end(log=logger)
-    def call_watermark(self, _):
-        """Process watermark command"""
-        set_preference(
-            "OPENBB_USE_WATERMARK", not get_current_user().preferences.USE_WATERMARK
-        )
-
-    @log_start_end(log=logger)
-    def call_cmdloc(self, _):
-        """Process cmdloc command"""
-        set_preference(
-            "OPENBB_USE_CMD_LOCATION_FIGURE",
-            not get_current_user().preferences.USE_CMD_LOCATION_FIGURE,
-        )
-
-    @log_start_end(log=logger)
-    def call_tbhint(self, _):
-        """Process tbhint command"""
-        if get_current_user().preferences.TOOLBAR_HINT:
-            console.print("Will take effect when running terminal next.")
-        set_preference(
-            "OPENBB_TOOLBAR_HINT", not get_current_user().preferences.TOOLBAR_HINT
         )
